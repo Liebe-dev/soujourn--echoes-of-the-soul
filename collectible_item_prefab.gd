87ea2a4b5bb04305fb1fcb2ffbf5f3703e9153c0
Rouinz <giabao2021.ngb@gmail.com>
2026-05-17 12:06:55 +0700

    Update collectible_item_prefab.gd

--- collectible_item_prefab.gd
+++ collectible_item_prefab.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
+#Gọi biến
 @onready var interaction = $Interact
 @onready var icon = $Icon
 @onready var notifier = $VisibleOnScreenNotifier2D
 var player : CharacterBody2D = null
 
-@export var detection_radius: float = 150.0
+@export var detection_radius: float = 150.0 #Khoảng cách có thể tương tác
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interaction.force_update_transform()
 	if icon and interaction and notifier:
+		#Chỉnh kích thước của notifier và vị trí của label Interact
 		var sprite_size = icon.texture.get_size() * icon.scale
 		var label_size = interaction.get_combined_minimum_size()
 		notifier.rect = Rect2(icon.position, sprite_size)
@@ -22,10 +24,12 @@ func _ready() -> void:
 		interaction.position = target_pos
 		interaction.visible = false
 		
+		#Lấy instance của Player để tính toán khoảng cách
 		player = get_tree().get_first_node_in_group("Player") as CharacterBody2D
 		notifier.screen_entered.connect(_on_screen_entered)
 		notifier.screen_exited.connect(_on_screen_exited)
 		
+		#Chỉ chạy func process khi vật phẩm có ở trên màn hình
 		if not notifier.is_on_screen():
 			set_process(false)
 		
@@ -33,6 +37,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not player:
 		return
+	#Khoảng cách giữa Player và Item
 	var distance = global_position.distance_to(player.position)
 	if distance <= detection_radius:
 		interaction.visible = true
