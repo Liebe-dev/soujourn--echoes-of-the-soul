991e28b204a77fe1b0fe96f62174f7bc9cb2df54
Rouinz <giabao2021.ngb@gmail.com>
2026-05-21 16:42:37 +0700

    created enemy prototype, working on clashing logic

--- scenes/testing/Enemies/enemy_weapon.gd
+++ scenes/testing/Enemies/enemy_weapon.gd
@@ -2,17 +2,31 @@ extends Node2D
 
 @onready var hitbox = $Hitbox/CollisionShape2D
 
+var indicator : Sprite2D = null
+var decision : int = 0
+var decided : float = false
+
 func _ready() -> void:
+	randomize()
+	indicator = %Indicator
+	indicator.visible = true
 	hitbox.disabled = true
 
 func _process(delta: float) -> void:
-	pass
+	if hitbox.disabled and not decided:
+		decided = true
+		decision = randi_range(0, 1)
+		if decision == 1:
+			indicator.visible = true
+	elif not hitbox.disabled and decided:
+		decided = false
+		indicator.visible = false
 
 func _on_timer_timeout() -> void:
 	hitbox.disabled = !hitbox.disabled
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
-	if "is_guarding" in body and "is_doing_action" in body:
+	if "is_guarding" in body and "is_doing_action" in body and decision == 0:
 		var enemy_direction = global_position.x - body.global_position.x
 		var is_facing_enemy = false
 		if (enemy_direction > 0 and body.facing_direction > 0):
@@ -26,3 +40,5 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 		elif body.has_method("take_damage"):
 			body.take_damage(20, global_position)
 		body.is_doing_action = false
+	elif "is_guarding" in body and "is_doing_action" in body and decision == 1:
+		pass#Clash logic here
---
+++ scenes/testing/Enemies/slime.gd
@@ -0,0 +1,11 @@
+extends EnemyPrototype
+
+func _ready() -> void:
+	super()
+	max_health = 200
+	speed = 150.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
